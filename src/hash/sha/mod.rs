4d383c9cb87b6fdb240e7c1557bd7d68904bae9b
= <=>
2023-10-30 11:26:57 -0400

    Removed SHA224

--- src/hash/sha/mod.rs
+++ src/hash/sha/mod.rs
@@ -2,12 +2,10 @@
 //!
 //! Implemented as defined in [RFC 6234](https://doi.org/10.17487/RFC6234)
 
-mod sha224;
 mod sha256;
 mod sha384;
 mod sha512;
 
-pub use sha224::*;
 pub use sha256::*;
 pub use sha384::*;
 pub use sha512::*;
