6b06122078cf3c1e4df54371ce95df0e048af3b4
Lance Hart <me@lancehart.ca>
2024-11-19 17:34:39 -0500

    Updated options

--- cli/src/command/hash/algorithm/positional.rs
+++ cli/src/command/hash/algorithm/positional.rs
@@ -10,25 +10,20 @@ impl Positional for HashAlgorithm {
         argument: Argument,
         info: &PositionalInfo<Self>,
     ) -> PositionalResult {
-        *this = Some(
-            match match argument.as_str() {
-                Ok(argument) => argument,
-                Err(error) => return PositionalResult::Error(error),
-            } {
-                "md5" => HashAlgorithm::MD5,
-                "sha1" => HashAlgorithm::SHA1,
-                "sha224" => HashAlgorithm::SHA224,
-                "sha256" => HashAlgorithm::SHA256,
-                "sha384" => HashAlgorithm::SHA384,
-                "sha512" => HashAlgorithm::SHA512,
-                _ => {
-                    return PositionalResult::Error(Error::invalid_positional_value(
-                        info.value,
-                        UnknownHashAlgorithm(argument.to_string()),
-                    ));
-                }
-            },
-        );
+        *this = Some(match argument.as_str()? {
+            "md5" => HashAlgorithm::MD5,
+            "sha1" => HashAlgorithm::SHA1,
+            "sha224" => HashAlgorithm::SHA224,
+            "sha256" => HashAlgorithm::SHA256,
+            "sha384" => HashAlgorithm::SHA384,
+            "sha512" => HashAlgorithm::SHA512,
+            _ => {
+                return PositionalResult::Error(Error::invalid_positional_value(
+                    info.value,
+                    UnknownHashAlgorithm(argument.to_string()),
+                ));
+            }
+        });
 
         PositionalResult::Next
     }
