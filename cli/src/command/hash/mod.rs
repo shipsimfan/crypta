47ba26c6ead73cd4821b10df87e65aeb08e110a2
Lance Hart <me@lancehart.ca>
2024-11-30 14:20:39 -0500

    Updated to use Command derive macro

--- cli/src/command/hash/mod.rs
+++ cli/src/command/hash/mod.rs
@@ -13,100 +13,12 @@ pub struct HashCommand {
     algorithm: HashAlgorithm,
 
     /// The set of files to hash
+    #[arg(min = 1)]
     files: Vec<PathBuf>,
 
     /// The number of times to read the input files into the hash
-    #[flag]
+    #[flag(short_name, default = DEFAULT_COUNT)]
     count: NonZeroUsize,
 }
 
-/*
-impl Command for HashCommand {
-    fn parse(source: &mut dyn ArgumentSource) -> Result<Self> {
-        const ALGORITHM_INFO: &PositionalInfo<HashAlgorithm> = &PositionalInfo {
-            value: "ALGORITHM",
-            min_count: 0,
-            max_count: 0,
-            default: None,
-        };
-        const FILES_INFO: &PositionalInfo<Vec<PathBuf>> = &PositionalInfo {
-            value: "FILE",
-            min_count: 1,
-            max_count: 0,
-            default: None,
-        };
-
-        const COUNT_INFO: &FlagInfo<NonZeroUsize> = &FlagInfo {
-            long_name: Some("--count"),
-            short_name: Some("-c"),
-            value: Some("COUNT"),
-            min_count: 0,
-            max_count: 0,
-            default: Some(|| unsafe { NonZeroUsize::new_unchecked(1) }),
-        };
-
-        let mut algorithm = None;
-        let mut files = None;
-
-        let mut current_positional = 0;
-
-        let mut count = None;
-
-        while let Some(argument) = source.next() {
-            if let Ok(argument) = argument.as_str() {
-                if argument.starts_with("--") {
-                    match &argument[2..] {
-                        "count" => count = Some(Flag::parse(source, COUNT_INFO, true)?),
-                        _ => return Err(Error::unknown_argument(argument.to_string())),
-                    }
-
-                    continue;
-                } else if argument.starts_with('-') && argument.len() > 1 {
-                    let mut chars = argument.chars();
-                    chars.next();
-                    for c in chars {
-                        match c {
-                            'c' => count = Some(Flag::parse(source, COUNT_INFO, false)?),
-                            _ => return Err(argparse::Error::unknown_argument(format!("-{}", c))),
-                        }
-                    }
-
-                    continue;
-                }
-            }
-
-            let result = match current_positional {
-                0 => Positional::parse(&mut algorithm, argument, ALGORITHM_INFO),
-                1 => Positional::parse(&mut files, argument, FILES_INFO),
-                _ => return Err(Error::unknown_argument(argument.to_string())),
-            };
-
-            match result {
-                PositionalResult::Continue => {}
-                PositionalResult::Next => current_positional += 1,
-                PositionalResult::Sub => {
-                    match current_positional {
-                        0 => Positional::sub(&mut algorithm, source),
-                        1 => Positional::sub(&mut files, source),
-                        _ => unreachable!(),
-                    }?;
-
-                    break;
-                }
-                PositionalResult::Error(error) => return Err(error),
-            };
-        }
-
-        let algorithm = Positional::unwrap(algorithm, ALGORITHM_INFO)?;
-        let files = Positional::unwrap(files, FILES_INFO)?;
-
-        let count = Flag::unwrap(count, COUNT_INFO)?;
-
-        Ok(HashCommand {
-            algorithm,
-            files,
-            count,
-        })
-    }
-}
-*/
+const DEFAULT_COUNT: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(1) };
